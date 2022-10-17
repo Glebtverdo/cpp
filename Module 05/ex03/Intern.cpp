3db1822f7e7285f8fa1c3e2bad9dbdd885c318a1
Vipir Earleen <vearleen@mi-s6.msk.21-school.ru>
2022-10-17 16:34:30 +0300

    Module 05 ex 03

--- Module 05/ex03/Intern.cpp
+++ Module 05/ex03/Intern.cpp
@@ -3,39 +3,52 @@
 #include"PresidentialPardonForm.hpp"
 #include"RobotomyRequestForm.hpp"
 
-Intern::Intern():{}
+Intern::Intern(){}
 Intern::~Intern(){}
 
 Intern::Intern(const Intern &srcForCopy){
     *this = srcForCopy;
 }
-Intern & Intern::operator=(const Intern &srcForAssigment){
-  return Intern();
+Intern  &Intern::operator=(const Intern &srcForAssigment){
+  (void)srcForAssigment;
+  return *this;
 }
 Form * Intern::makeForm(std::string formName, std::string formTarget){
   int i = 0;
   const std::string formNames[3] = {
-    "robotomy request",
     "shrubbery creation",
-    "presidental paedon"
+    "presidental pardon",
+    "robotomy request"
   };
-  const Form *forms[3] = {
-    &ShrubberyCreationForm(),
-    &PresidentialPardonForm(),
-    &RobotomyRequestForm()
+  Form *forms[3] = {
+    new ShrubberyCreationForm(formTarget),
+    new PresidentialPardonForm(formTarget),
+    new RobotomyRequestForm(formTarget)
   };
+
+  Form *tmp = NULL;
+
   try
   {
-    while (i < 4)
+    while (i < 3)
     {
-      if (i == 3)
+      if(formNames[i].compare(formName) == 0)
+        tmp = forms[i];
+      else
+        delete forms[i];
+      i++;
+      if (i == 3 && !tmp)
         throw Intern::UnknownFormName();
-      
     }
   }
-  catch(const std::exception& e)
+  catch(const Intern::UnknownFormName &e)
   {
     std::cerr << e.what() << '\n';
+    return NULL;
   }
-  
+  return tmp;
+}
+
+const char *Intern::UnknownFormName::what() const throw(){
+  return "unknown form type";
 }
--- Module 05/ex03/main.cpp
+++ Module 05/ex03/main.cpp
@@ -1,24 +1,27 @@
 #include"ShrubberyCreationForm.hpp"
 #include"PresidentialPardonForm.hpp"
 #include"RobotomyRequestForm.hpp"
-
+#include"Intern.hpp"
 
 int main()
 {
-    Bureaucrat bur("sam", 50);
-    ShrubberyCreationForm one("order");
-    RobotomyRequestForm robo("order");
-    PresidentialPardonForm pres("order");
+    const std::string strsToTest[] = {
+        "shrubbery creation",
+        "presidental pardon",
+        "robotomy request",
+        "hrubbery creation",
+        "residental pardon",
+        "obotomy request"
+    };
+    Intern machine;
+    for (size_t i = 0; i < 6; i++)
+    {
+        const Form *tmp = machine.makeForm(strsToTest[i], "target");
+        if(tmp){
+            tmp->action();
+            delete tmp;
+        }
+    }
     
-    Bureaucrat mainBur("Master", 1);
-
-    bur.executeForm(one);
-    bur.executeForm(robo);
-    bur.executeForm(pres);
-    mainBur.executeForm(one);
-    mainBur.executeForm(robo);
-    mainBur.executeForm(pres);
-    // Form order1("order1", 155, 0);
-
     return 0;
 }
